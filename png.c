94eec1e0a2c45ecc08c4068106c94f9e78e32c90
Zach Schneider <zschneider@berkeley.edu>
2014-02-26 14:41:16 -0800

    Successfully calculates length of chunk, working on matching

--- png.c
+++ png.c
@@ -7,78 +7,67 @@
 long int chunk_length(char* buffer) {
   /*HOW IT WORKS: Take our initial buffer, store the values into a new buffer
   with leading 0s using the command sprintf. Then use strtol to convert
-  our new string "buffer2" into int value using base 16."
-
+  our new string "buffer2" into int value using base 16."*/
 
   char *buffer2;
-  //int *digits;
-  //digits = (int *) malloc(4*sizeof(int));
   printf("Buffer is: %X %X %X %X\n", buffer[0], buffer[1], buffer[2], buffer[3]);
   buffer2 = (char *) malloc(4*sizeof(char));
-  sprintf(buffer2, "%02X%02X%02X%02X", buffer[0], buffer[1], buffer[2], buffer[3]);
-  long int length = 0;
+  sprintf(buffer2, "%02X%02X%02X%02X",(unsigned char) buffer[0],(unsigned char) buffer[1],(unsigned char) buffer[2],(unsigned char) buffer[3]);
   printf("Buffer2 is: %s\n", buffer2); 
   char *end;
-  long int lint0 = strtol(buffer2, &end, 16);
-  //long int lint1 = strtol(end, &end, 0);
-  //long int lint2 = strtol(end, &end, 0);
-  //long int lint3 = strtol(end, &end, 0);
-  
+  long int lint0 = strtol(buffer2, &end, 16);  
   printf("Value is: %ld\n", lint0);
-  //printf("Values are: %ld, %ld, %ld, %ld\n", lint0, lint1, lint2, lint3);  
-  //length = lint3 + (lint2 * 16) + (lint1 * 256) + (lint0 * 2096);
-  //length = atoi(buffer2);
-  printf("atoi length: %ld\n", length);
-  //free(digits);
-  //free(buffer2);
+  free(buffer2);
   getchar();
-  return length;
+  return lint0;
 }
 
 
 int analyze_chunks(FILE *f) {
-  //char tEXt[4] = {0x74, 0x45, 0x58, 0x74};
-  //char zTXt[4] = {0x7A, 0x54, 0x58, 0x74};
-  //char tIME[4] = {0x74, 0x49, 0x4D, 0x45};
+  char tEXt[4] = {0x74, 0x45, 0x58, 0x74};
+  char zTXt[4] = {0x7a, 0x54, 0x58, 0x74};
+  char tIME[4] = {0x74, 0x49, 0x4d, 0x45};
   /* Idea for proceeding: we need to go through chunks of code. Chunks take the form:
      length of data portion of chunk (4 bytes) chunktype (4bytes) data (variable bytes) checksum (4 bytes) 
      So process first 8 bytes of next chunk. If the last 4 bytes match chunktypes above, then process
      and output data portion (after verifying checksum). Otherwise skip ahead the length of the data 
      + 4 bytes. */
   /* This function only analyzes 8 bytes at a time */
-  char length[4];
+  char buffer[8];
   //char chunktype[4];
   int chunklength;  
-  while(fread(length, 1, 4, f) == 4) {
+  while(fread(buffer, 1, 8, f) == 8) {
     //int i = 0;
-    chunklength = chunk_length(length);
+    chunklength = chunk_length(buffer);
     //chunklength = 0;
     printf("This chunk has length %d\n", chunklength);
-    //while ( i < 4 ) {
-      //printf("%X:",(unsigned char)length[i] );
+    int i = 0;
+    while ( i < 8 ) {
+      printf("%X:",(unsigned char) buffer[i] );
       //printf("%x",tEXt[i-4]);
       //printf("%x",zTXt[i-4]);
       //printf("%x",tIME[i-4]);
-      //i++;
-    //}
-    //if (strcmp(buffer+4, tEXt) == 0) {
-    //  printf("We gots a tEXt.");
+      i++;
+    }
+    printf("\n");
+    if (strcmp(buffer+4, tEXt) == 0) {
+      printf("We gots a tEXt.\n");
       /* atoi will take the first four characters and output an integer 
 	 but apparently this doesnt work at all */
     //  fseek(f, chunklength, SEEK_CUR);
-    //}
-    //else if (strcmp(buffer+4, zTXt) == 0) {
-    //  printf("We gots a zTXt. Decompress that shit.");
+    }
+    else if (strcmp(buffer+4, zTXt) == 0) {
+      printf("We gots a zTXt. Decompress that shit.\n");
     //  fseek(f, chunklength, SEEK_CUR);
-    //}
-    //else if (strcmp(buffer+4, tIME) == 0) {
-    //  printf("What tIME is it? PARTY TIME!");
+    }
+    else if (strcmp(buffer+4, tIME) == 0) {
+      printf("What tIME is it? PARTY TIME!\n");
     //  fseek(f, chunklength, SEEK_CUR);
-    //} else {   
+    } else {   
       //getchar();      
-      //printf("Who cares? Moving ahead %d spaces\n", moveahead);
-      //fseek(f,chunklength, SEEK_CUR);
-      //}
+      printf("Who cares?\n");
+    }
+    fseek(f,chunklength+4, SEEK_CUR);
     //getchar();
   }
   if (feof(f)) {
