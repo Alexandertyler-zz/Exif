f1f2a558d17d7bf789698c75a7492e57f7287a33
Zach Schneider <zschneider@berkeley.edu>
2014-02-26 13:37:47 -0800

    working on reading hex

--- png.c
+++ png.c
@@ -4,60 +4,72 @@
 #include <zlib.h>
 #include "png.h"
 
+long int chunk_length(char* buffer) {
+  char *buffer2;
+  //int *digits;
+  //digits = (int *) malloc(4*sizeof(int));
+  buffer2 = (char *) malloc(4*sizeof(char));
+  int length = 0;
+  /* Converts the hex in buffer to ints in char array */
+  sprintf(buffer2, "%d%d%d%d", buffer[0], buffer[1], buffer[2], buffer[3]);
+  /* Converts the ints in char array to int array */
+  //printf("%ld\n", strtol(buffer, NULL, 0));
+  //printf("%X\n", buffer);
+  //sscanf(buffer2, "%d %d %d %d", &digits[0], &digits[1], &digits[2], &digits[3]);
+  //printf("%d:%d:%d:%d\n", digits[0], digits[1], digits[2], digits[3]);
+  //length = digits[0] + (digits[1] * 16) + (digits[2] * 256) + (digits[3] * 2096);
+  length = atoi(buffer2);
+  printf("atoi length: %d\n", length);
+  //free(digits);
+  free(buffer2);
+  return length;
+}
+
+
 int analyze_chunks(FILE *f) {
-  char tEXt[4] = {0x74, 0x45, 0x58, 0x74};
-  char zTXt[4] = {0x7A, 0x54, 0x58, 0x74};
-  char tIME[4] = {0x74, 0x49, 0x4D, 0x45};
+  //char tEXt[4] = {0x74, 0x45, 0x58, 0x74};
+  //char zTXt[4] = {0x7A, 0x54, 0x58, 0x74};
+  //char tIME[4] = {0x74, 0x49, 0x4D, 0x45};
   /* Idea for proceeding: we need to go through chunks of code. Chunks take the form:
      length of data portion of chunk (4 bytes) chunktype (4bytes) data (variable bytes) checksum (4 bytes) 
      So process first 8 bytes of next chunk. If the last 4 bytes match chunktypes above, then process
      and output data portion (after verifying checksum). Otherwise skip ahead the length of the data 
      + 4 bytes. */
   /* This function only analyzes 8 bytes at a time */
-  char buffer[8];
-  //char length[4];
-    
-  while(fread(buffer, 1, 8, f) == 8) {
+  char length[4];
+  //char chunktype[4];
+  int chunklength;  
+  while(fread(length, 1, 4, f) == 4) {
     int i = 0;
-    
-    while ( i < 8 ) {
-      printf("%X:",(unsigned char)buffer[i] );
+    chunklength = chunk_length(length);
+    //chunklength = 0;
+    printf("This chunk has length %d\n", chunklength);
+    while ( i < 4 ) {
+      printf("%X:",(unsigned char)length[i] );
       //printf("%x",tEXt[i-4]);
       //printf("%x",zTXt[i-4]);
       //printf("%x",tIME[i-4]);
       i++;
     }
-    printf("\n");
-    if (strcmp(buffer+4, tEXt) == 0) {
-      printf("We gots a tEXt.");
+    //if (strcmp(buffer+4, tEXt) == 0) {
+    //  printf("We gots a tEXt.");
       /* atoi will take the first four characters and output an integer 
 	 but apparently this doesnt work at all */
-      fseek(f, atoi(buffer), SEEK_CUR);
-    }
-    else if (strcmp(buffer+4, zTXt) == 0) {
-      printf("We gots a zTXt. Decompress that shit.");
-      fseek(f, atoi(buffer), SEEK_CUR);
-    }
-    else if (strcmp(buffer+4, tIME) == 0) {
-      printf("What tIME is it? PARTY TIME!");
-      fseek(f, atoi(buffer), SEEK_CUR);
-    } else {
-      char bytes[4];
-      memcpy(bytes, buffer, 4);
-      string str(bytes);
-      printf("String, %s\n", string);
-      
-      int moveahead = 4;
-      long int li1;
-      char * pEnd;
-      li1 = strtol (string, &pEnd, 0);
-      printf("The li1, %ld\n", li1);
-      getchar();
-      //check out sscanf
-      printf("Who cares? Moving ahead %d spaces\n", moveahead);
-      fseek(f, moveahead, SEEK_CUR);
-    }
-    getchar();
+    //  fseek(f, chunklength, SEEK_CUR);
+    //}
+    //else if (strcmp(buffer+4, zTXt) == 0) {
+    //  printf("We gots a zTXt. Decompress that shit.");
+    //  fseek(f, chunklength, SEEK_CUR);
+    //}
+    //else if (strcmp(buffer+4, tIME) == 0) {
+    //  printf("What tIME is it? PARTY TIME!");
+    //  fseek(f, chunklength, SEEK_CUR);
+    //} else {   
+      //getchar();      
+      //printf("Who cares? Moving ahead %d spaces\n", moveahead);
+      //fseek(f,chunklength, SEEK_CUR);
+      //}
+    //getchar();
   }
   if (feof(f)) {
     printf("EOF reached.");
